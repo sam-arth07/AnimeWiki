bbc230f52154b6f216774c89d7997d41613305ea
Samarth Chaplot <samarthchaplot7@gmail.com>
2025-07-17 17:47:03 +0530

    Final Commit for now

--- app/src/main/java/com/example/animewiki/util/Constants.kt
+++ app/src/main/java/com/example/animewiki/util/Constants.kt
@@ -9,7 +9,8 @@ object Constants {
     const val COMPLETED_PREFERENCE_KEY = "completed_onboarding"
     const val PREFERENCES_NAME = "anime_preferences"
 
-    const val BASE_URL = "http://10.0.2.2:8080"
+//    const val BASE_URL = "http://10.0.2.2:8080"
+    const val BASE_URL = "https://animeserver-cm3n.onrender.com/"
     const val HEROES_PER_PAGE = 3
     const val MIN_BACKGROUND_IMAGE_FRACTION = 0.4f
 }
